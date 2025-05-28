b13a3090f8284701d5d8871999adc6445215181e
Yasin Cesur <cesuryasin83@gmail.com>
2025-05-28 22:03:24 +0300

    first commit

--- src/components/AIFace.js
+++ src/components/AIFace.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import { useFrame } from 'react-three-fiber';
+import { useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import * as THREE from 'three';
 
